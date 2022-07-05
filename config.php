860b6fd7b177f98cbdd55cb41e26a40170e143ff
ammutxyz <103256226+ammutxyz@users.noreply.github.com>
2022-07-05 18:24:13 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+   define('DB_SERVER', 'localhost:3036');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', '');
+   define('DB_DATABASE', '');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
